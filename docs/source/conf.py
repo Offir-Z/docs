47a13f93ade7b54f80fccfe009637ba36db3a7e6
Offir-Z <99115392+Offir-Z@users.noreply.github.com>
2022-10-31 22:34:31 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Astrix'
+copyright = '2022, Astrix Security'
+author = 'Astrix Docs'
 
 release = '0.1'
 version = '0.1.0'
